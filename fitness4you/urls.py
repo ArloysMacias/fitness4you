72fd58cffb8e880cfbe538d7a3aeac19b175b9a9
ArloysMacias <arloys.rojas@atg.se>
2020-11-16 22:24:55 +0100

    Implemented home page sections

--- fitness4you/urls.py
+++ fitness4you/urls.py
@@ -15,9 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# from django.conf.urls import url, include
+# from django.contrib import admin
+# from django.conf import settings
+# from django.conf.urls.static import static
+#
+# urlpatterns = [
+#                   url(r'', include('home.urls')),
+#                   url(r'^admin/', admin.site.urls),
+#                   url(r'^accounts/', include('allauth.urls')),
+#               ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

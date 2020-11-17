de9211f76a1b066ac41b7e4e4347890de209910b
ArloysMacias <arloys.rojas@atg.se>
2020-11-17 20:55:05 +0100

    Created navbar in the desktop and tablet view

--- home/static/home/js/materialize.js
+++ home/static/home/js/materialize.js
@@ -1,23 +1,16 @@
 $(document).ready(function(){
+
+    //Materialize
+
+    //NavBar
     $('.collapsible').collapsible();
     $('select').material_select();
     document.querySelectorAll('.select-wrapper').forEach(t => t.addEventListener('click', e=>e.stopPropagation())) // fixes first click
     $(".button-collapse").sideNav();
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15, // Creates a dropdown of 15 years to control year,
-        today: 'Today',
-        clear: 'Clear',
-        close: 'Ok',
-        closeOnSelect: false, // Close upon selecting a date,
-        defaultTime: 'now'
-    });
-    $('.datepicker').on('mousedown',function(event){ event.preventDefault(); })
-    $('.carousel').carousel({
-        duration:120,
-        dist:-150,
-        numVisible:6
-    });
-    $('.modal').modal();
+    $('.dropdown-buttonh').dropdown();
     $('.parallax').parallax();
+
+    // //Toast
+    // $('.toast').toast('show');
+
 });

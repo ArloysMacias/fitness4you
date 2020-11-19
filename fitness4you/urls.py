99eda684abf7135fe1d25b61f5369d466ee053e3
ArloysMacias <arloys.rojas@atg.se>
2020-11-19 17:12:04 +0100

    Database loaded and the base view of products created

--- fitness4you/urls.py
+++ fitness4you/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
+    path('products/', include('products.url'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # from django.conf.urls import url, include

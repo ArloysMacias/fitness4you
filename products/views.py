08af16e32f92e302a76b953e5b5086a401e9a081
ArloysMacias <arloys.rojas@atg.se>
2020-11-20 05:20:04 +0100

    Created view for product brands filter

--- products/views.py
+++ products/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Product
+from django.contrib import messages
+from django.db.models import Q
+from .models import Category
 
 
 # Create your views here.
@@ -7,10 +10,28 @@ def all_products(request):
     """A view to show all products"""
 
     products = Product.objects.all()
+    search = None
+    brand = None
+
+    if request.GET:
+        if 'search' in request.GET:
+            search = request.GET['search']
+            if not search:
+                messages.error(request, "Nothing is typed")
+                return redirect(reverse('products'))
+            queries = Q(product_name__icontains=search) | Q(product_description__icontains=search)
+            products = products.filter(queries)
+
+    if request.GET:
+        if 'brand_name' in request.GET:
+            brandlist = request.GET['brand_name'].split(',')
+            # queries = Q(products__brand_name__in=brandlist)
+            products = products.filter(brand_name__in=brandlist)
 
     context = {
         'products': products,
+        'search': search,
+        'brand': brand
     }
 
     return render(request, 'products/products.html', context)
-

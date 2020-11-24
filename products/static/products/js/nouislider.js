7b9f38ccda661478d03ce689decb30da33a1b6ea
ArloysMacias <arloys.rojas@atg.se>
2020-11-24 15:28:59 +0100

    Added tooltips and format to the noUiSlider in the price section

--- products/static/products/js/nouislider.js
+++ products/static/products/js/nouislider.js
@@ -4,24 +4,48 @@ $(document).ready(function () {
         start: [document.getElementById('skip-value-lower').value, document.getElementById('skip-value-upper').value],
         connect: true,
         step: 2,
-        orientation: 'horizontal', // 'horizontal' or 'vertical'
+        orientation: 'horizontal',
+        tooltips: [wNumb({
+            prefix: '$',
+            decimals: 1
+        }), wNumb({
+            prefix: '$',
+            decimals: 1
+        })],
         range: {
             'min': 0,
             'max': 400
         },
-        // pips: {
-        //     format: wNumb({
-        //         prefix: '$ ',
-        //         decimals: 3
-        //     })
-        // }
+        pips: {
+            mode: 'range',
+            density: 2,
+            format: wNumb({
+                prefix: '$',
+                decimals: 1
+            })
+        },
+        // format: wNumb({
+        //     decimals: 3,
+        //     thousand: '.',
+        //     suffix: ' (US $)'
+        // })
     });
-    var skipValues = [
+    var rangeValues = [
         document.getElementById('skip-value-lower'),
         document.getElementById('skip-value-upper')
     ];
 
     slider.noUiSlider.on('update', function (values, handle) {
-        skipValues[handle].value = values[handle];
+        rangeValues[handle].value = values[handle];
     })
+
+    var inputFormat_lower = document.getElementById('skip-value-lower');
+    var inputFormat_upper = document.getElementById('skip-value-lower');
+    inputFormat_lower.addEventListener('change', function () {
+        slider.noUiSlider.set(this.value);
+    });
+    inputFormat_upper.addEventListener('change', function () {
+        slider.noUiSlider.set(this.value);
+    });
+
 });

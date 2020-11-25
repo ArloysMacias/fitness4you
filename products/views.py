3d9e10cada064db343ea37df21efae54334dcdd2
ArloysMacias <arloys.rojas@atg.se>
2020-11-25 03:04:08 +0100

    Captured the session in the brand and sent to the template

--- products/views.py
+++ products/views.py
@@ -1,10 +1,8 @@
-from django.db.models.functions import Lower
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.views.generic import ListView
+from django.shortcuts import render, redirect
 from .models import Product, Category
 from django.contrib import messages
 from django.db.models import Q
-from .filters import ProductFilter
+from django.http import HttpResponse, response
 
 
 def is_valid(form_parameter, request):
@@ -17,6 +15,15 @@ def is_valid(form_parameter, request):
     else:
         messages.error(request, f"The criteria {form_parameter} is not valid")
         return redirect('products')
+
+
+def updated_filter(parameter, request):
+    if request.GET:
+        if request.session[parameter]:
+            result = request.session[parameter]
+        else:
+            result = request.GET[parameter]
+    return result
 
 
 # Create your views here.
@@ -32,9 +39,11 @@ def all_products(request):
 
     brands_column = products.values_list('brand_name', flat=True).distinct()
     brand_selected = {}
+    brand_name = None
 
     list_categories_friendly_name = categories.values_list('friendly_name', flat=True).distinct()
     category_selected = None
+    category_friendly = None
 
     price_column = products.values_list('price', flat=True)
     lower_price = 0
@@ -44,31 +53,38 @@ def all_products(request):
     #     if is_valid('search', request):
     #         search = request.GET['search']
     #         products = products.filter(Q(product_name__icontains=search) | Q(product_description__icontains=search))
-
+    #
     # current_brands = brands.filter(brand_name__in=checked_brand)
 
     if request.GET:
 
         if is_valid('overall_rating', request):
-            overall_rating_selected = float(request.GET['overall_rating'])
+            if overall_rating_selected in request.COOKIES:
+                overall_rating_selected = request.COOKIES[overall_rating_selected]
+            else:
+                overall_rating_selected = float(request.GET['overall_rating'])
             products = products.filter(overall_rating__gte=overall_rating_selected)
             overall_rating_filtered = overall_rating_column.filter(overall_rating__gte=overall_rating_selected)
 
         if is_valid('brand_name', request):
+            # brand_name = updated_filter('brand_name', request)
             brand_name = request.GET['brand_name']
             products = products.filter(brand_name__icontains=brand_name)
+            products = products.filter(brand_name__icontains=brand_name)
             brand_selected = brands_column.filter(brand_name__icontains=brand_name).first()
+            request.session['brand_name'] = brand_selected
 
         if is_valid('category', request):
+            brand_name = updated_filter('brand_name', request)
+            brand_selected = brands_column.filter(brand_name__icontains=brand_name).first()
             category_friendly = request.GET['category']
             products = products.filter(category__friendly_name__exact=category_friendly)
             category_selected = list_categories_friendly_name.filter(friendly_name__exact=category_friendly).first()
 
         if is_valid('skip-value-lower', request):
-            if is_valid('skip-value-upper', request):
-                lower_price = float(request.GET['skip-value-lower'])
-                upper_price = float(request.GET['skip-value-upper'])
-                products = products.filter(Q(price__gte=lower_price) & Q(price__lte=upper_price))
+            lower_price = float(request.GET['skip-value-lower'])
+            upper_price = float(request.GET['skip-value-upper'])
+            products = products.filter(Q(price__gte=lower_price) & Q(price__lte=upper_price))
 
     context = {
         'products': products,

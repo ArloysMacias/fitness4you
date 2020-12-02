3d413213c3b67994b7d9e3aa622b8f51a9647753
ArloysMacias <arloys.rojas@atg.se>
2020-12-02 18:29:23 +0100

    Filled the list of products in the shopping cart with their respective prices and total price

--- bag/contexts.py
+++ bag/contexts.py
@@ -7,7 +7,8 @@ from products.models import Product
 
 def bag(request):
     bag_items = []
-    total = 0
+    total_item_price = 0
+    sum_total = 0
     amount_of_products = 0
 
     bag = request.session.get('bag', {})
@@ -22,18 +23,19 @@ def bag(request):
         product = get_object_or_404(Product, pk=item_id)
         digits = [int(x) for x in quantity]
         quantity = sum(digits)
-        total += Decimal(quantity) * Decimal(product.price)
+        total_item_price = Decimal(quantity) * Decimal(product.price)
+        sum_total += total_item_price
         amount_of_products += int(quantity)
         bag_items.append({
             'id': item_id,
             'quantity': quantity,
             'product': product,
+            'total_item_price': total_item_price,
         })
-    print(bag_items)
 
     context = {
         'bag_items': bag_items,
-        'total': total,
+        'sum_total': sum_total,
         'amount_of_products': amount_of_products,
     }
     return context

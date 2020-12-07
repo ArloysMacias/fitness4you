4624e320d71a63b0f88f26a755c9854ddf84dd84
ArloysMacias <arloys.rojas@atg.se>
2020-12-08 00:54:00 +0100

    Added fomrs.py to the checkout app

---
+++ checkout/forms.py
@@ -0,0 +1,32 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'country', 'city', 'address', 'postcode')
+
+    def __init__(self, *args, **kwargs):
+
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country, State or Locality',
+            'city': 'City',
+            'address': 'Address',
+            'postcode': 'Postal Code',
+        }
+        """Set the cursor to the full name field when the user loads the page"""
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+
+        for field in self.fields:
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False

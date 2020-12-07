852244dc806e726c5fd623bf2599112bead23861
ArloysMacias <arloys.rojas@atg.se>
2020-12-08 00:22:50 +0100

    Created signals to update total_to_pay when a product update/create/deleted

---
+++ checkout/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import ProductOrder
+
+
+@receiver(post_save, sender=ProductOrder)
+def update_on_save(sender, instance, created, **kwargs):
+    """Update total_to_pay when a product update/create"""
+    instance.order.update_price()
+
+
+@receiver(post_delete, sender=ProductOrder)
+def update_on_delete(sender, instance, **kwargs):
+    """Update total_to_pay when a product delete"""
+    instance.order.update_price()

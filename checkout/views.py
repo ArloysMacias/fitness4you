0c8edc8d123db825ba7c327f57473c442fa8970f
ArloysMacias <arloys.rojas@atg.se>
2020-12-09 23:02:42 +0100

    Stylized allauth base template

--- checkout/views.py
+++ checkout/views.py
@@ -6,6 +6,7 @@ from .forms import OrderForm
 def checkout(request):
     bag = request.session.get('bag', {})
     if not bag:
+        return redirect(reverse('shopping_bag'))
         messages.error(request, "There is nothing in the bag at the moment")
 
     order_form = OrderForm()
@@ -13,5 +14,4 @@ def checkout(request):
     context = {
         'order_form': order_form,
     }
-
     return render(request, template, context)

11bad6e5bffca48ba2f5c87f12b485184ef6ed49
ArloysMacias <arloys.rojas@atg.se>
2020-12-14 15:54:31 +0100

    Updated allauth templates, dropdown in the navbar and tested profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import UserProfile
 
 
 def profile(request):
     """Display user's profile"""
+
+    profiles = UserProfile.objects.all()
+    print(profiles)
+
+    the_profile = profiles.filter(full_name_default__iexact=request.user)
+
+    print(the_profile)
+
     template = 'profiles/profile.html'
-    context = {}
+    context = {
+        'profile': the_profile
+    }
 
     return render(request, template, context)

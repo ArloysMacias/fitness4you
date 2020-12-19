9850dec0ec98ee4bd96bf37e74267898230adf3e
ArloysMacias <arloys.rojas@atg.se>
2020-12-19 14:57:33 +0100

    Deployment: Changed the default database to postgres and created the application in heroku

--- fitness4you/settings.py
+++ fitness4you/settings.py
@@ -8,6 +8,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,8 +18,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
-#SECRET_KEY ='ss'
+#SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY ='ss'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -133,6 +134,7 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators

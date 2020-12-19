836fadbb39e2cfa90bae55aece66c1eff4bf79d9
ArloysMacias <arloys.rojas@atg.se>
2020-12-19 15:16:29 +0100

    Deployment: Setted Gunicorn

--- fitness4you/settings.py
+++ fitness4you/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY ='ss'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1:8000', '127.0.0.1', '4d317cc4e698.ngrok.io']
+ALLOWED_HOSTS = ['fitness4you.herokuapp.com', '127.0.0.1:8000', '127.0.0.1', '4d317cc4e698.ngrok.io']
 
 # Application definition
 
@@ -128,12 +128,17 @@ WSGI_APPLICATION = 'fitness4you.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation

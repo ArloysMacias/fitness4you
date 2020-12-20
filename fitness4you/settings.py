84c57cc1807d8e14f3e45b92861496b8e97eb0cf
ArloysMacias <arloys.rojas@atg.se>
2020-12-20 17:02:38 +0100

    Setted the GOOGLE_APPLICATION_CREDENTIALS to store media files

--- fitness4you/settings.py
+++ fitness4you/settings.py
@@ -179,13 +179,12 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 if 'DEVELOPMENT' in os.environ:
-    # Simplified static file serving.
+
+
     # https://warehouse.python.org/project/whitenoise/
 
     STATIC_URL = '/static/'
@@ -202,17 +201,19 @@ if 'DEVELOPMENT' in os.environ:
 
     MEDIA_ROOT = os.path.join(BASE_DIR, "live-static", "media-root")
 
+    DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
+    GS_BUCKET_NAME = 'fitness4you'
+
 else:
     STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
     ]
 
-    #STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 
 # Stripe
 STRIPE_CURRENCY = 'usd'

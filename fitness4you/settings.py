4e7532738fe166371e2b66471fe1cd208eb973ca
ArloysMacias <arloys.rojas@atg.se>
2020-12-22 17:46:59 +0100

    Added USE_CLOUDINARY variable to heroku

--- fitness4you/settings.py
+++ fitness4you/settings.py
@@ -208,7 +208,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
-if 'DEVELOPMENT' in os.environ:
+if 'USE_CLOUDINARY' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 else:

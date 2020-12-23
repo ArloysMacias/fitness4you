85dfabd63e20cd09db1d866a9762cb52fafd515b
ArloysMacias <arloys.rojas@atg.se>
2020-12-23 20:01:01 +0100

    Detleted restriction to see profile if user is not superuser and allow user to see their profile

--- profiles/views.py
+++ profiles/views.py
@@ -8,10 +8,11 @@ from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
 
+@login_required
 def profile(request):
     """Display user's profile"""
 
-    if not request.user.is_superuser:
+    if not request.user.is_authenticated:
         messages.error(request, "Sorry, you don't seem to have clearance to do that 🤭")
         return redirect(reverse('products'))
 

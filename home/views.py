d83f517a43080767355f90791fa21cc864e92a0a
ArloysMacias <arloys.rojas@atg.se>
2020-12-23 10:59:39 +0100

    Fixed size of the cards and hidding exclusive cards from the home page if user is not authenticated

--- home/views.py
+++ home/views.py
@@ -7,8 +7,20 @@ from products.models import Product
 def index(request):
     """A view to return the index page"""
     products_list = Product.objects.all()
-    popular_products = sorted(products_list, key=lambda x: x.overall_rating, reverse=True)[:3]
+    popular_products = sorted(products_list, key=lambda x: x.overall_rating, reverse=True)
+
+    list_to_show = []
+    list_popular_exclusive = []
+    for product in products_list:
+        if product.exclusive:
+            list_popular_exclusive.append(product)
+
+    list_to_show = list(filter(lambda product: product not in list_popular_exclusive, popular_products))
+    if request.user.is_authenticated:
+        list_to_show = popular_products  # ALL
+
+
     context = {
-        'products': popular_products,
+        'products': list_to_show[:3],
     }
     return render(request, 'home/index.html', context)

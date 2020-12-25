49d0f0e1e1796ec4cff2b51071320050bf66d0af
ArloysMacias <arloys.rojas@atg.se>
2020-12-25 16:42:10 +0100

    Hiding maps on medium and small devices

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -6,7 +6,7 @@ function initializeMaterialize() {
     $('.collapsible').collapsible();
     $('select').material_select();
     $(".button-collapse").sideNav();
-    $('.dropdown-buttonh').dropdown();
+    $('.dropdown-button').dropdown();
     $('.parallax').parallax();
     $('.modal').modal();
     document.querySelectorAll('.select-wrapper').forEach(t => t.addEventListener('click', e=>e.stopPropagation())) ;// fixes first click

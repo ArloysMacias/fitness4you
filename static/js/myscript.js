a21034a5fa9a2e8257d53e3a406a6e056ad23691
ArloysMacias <arloys.rojas@atg.se>
2020-12-25 16:29:49 +0100

    Stoping double click in dropdown buttons: stopPropagation

--- static/js/myscript.js
+++ static/js/myscript.js
@@ -5,9 +5,9 @@ $(document).ready(function(){
 function initializeMaterialize() {
     $('.collapsible').collapsible();
     $('select').material_select();
-    document.querySelectorAll('.select-wrapper').forEach(t => t.addEventListener('click', e=>e.stopPropagation())) ;// fixes first click
     $(".button-collapse").sideNav();
     $('.dropdown-buttonh').dropdown();
     $('.parallax').parallax();
     $('.modal').modal();
+    document.querySelectorAll('.select-wrapper').forEach(t => t.addEventListener('click', e=>e.stopPropagation())) ;// fixes first click
 }

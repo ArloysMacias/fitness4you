ba2e7cd58b482b6b3198e47cb031d1d540c8a806
ArloysMacias <arloysmaciasrojas@gmail.com>
2023-03-25 23:08:25 +0000

    Fill the database

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ set -o errexit  # exit on error
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py loaddata fixtures/*.json
 python manage.py createsuperuser --username $SU_USERNAME --email $SU_EMAIL --password $SU_PASSWORD

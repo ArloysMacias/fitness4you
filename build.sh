73e0ce39f155f0d62135ee7f805185f2e537d1de
ArloysMacias <arloysmaciasrojas@gmail.com>
2023-03-26 01:30:15 +0000

    Update build file

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit  # exit on error
 
 pip install -r requirements.txt
-python manage.py flush
+python manage.py flush --no-input
 python manage.py collectstatic --no-input 
 python manage.py migrate
 echo "$SU_PASSWORD" | python manage.py createsuperuser --username $SU_USERNAME --email $SU_EMAIL --no-input
